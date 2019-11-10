f51083263b1cbff496c55c641132be2d78a536f4
Ian Hansson <ian.hansson@gmail.com>
2019-11-10 07:34:35 +0000

    complete rewrite

--- README.md
+++ README.md
@@ -4,11 +4,29 @@
 
 AIV is a simple library for toggling classes when elements are inview. With support for offseting when the element is in-view, delaying the animation, or triggering classes on child elements in sequence.
 
+## Init
+
+Include the library and init using default options or given options
+
+```html
+<script type="text/javascript" src="aiv.min.js"></script>
+<script type="text/javascript">aiv.init()</script>
+```
+
+```html
+<script type="text/javascript" src="aiv.min.js"></script>
+<script type="text/javascript">aiv.init({
+	in_cls: 'in-view',
+	out_cls: 'out-of-view',
+	throttle: 50
+})</script>
+```
+
 ## Getting Started
 
 In-view classes can be toggled using an html attribute `aiv` `aiv-...` interface. Simply calling aiv will trigger the built in `in-view` and `out-of-view` classes.
 
-```
+```html
 <div aiv>Basic Toggle</div>
 ```
 
@@ -16,7 +34,7 @@ In-view classes can be toggled using an html attribute `aiv` `aiv-...` interface
 
 You can add custom classes using `aiv-cls` and `aiv-out_cls`.
 
-```
+```html
 <div aiv aiv-cls="yellow" aiv-out_cls="red">Yellow when in. Red when out.</div>
 ```
 
@@ -24,7 +42,7 @@ You can add custom classes using `aiv-cls` and `aiv-out_cls`.
 
 By default an element will only trigger classes once. If you want the animation to repeat when coming back to it use `aiv-repeat`.
 
-```
+```html
 <div aiv aiv-cls="yellow" aiv-out_cls="red" aiv-repeat>Repeating transition.</div>
 ```
 
@@ -32,7 +50,7 @@ By default an element will only trigger classes once. If you want the animation 
 
 Setting `aiv-delay` will hold the transition to in-view by the specified number in milliseconds.
 
-```
+```html
 <div aiv aiv-delay="500">Repeating transition.</div>
 ```
 
@@ -40,7 +58,7 @@ Setting `aiv-delay` will hold the transition to in-view by the specified number 
 
 To make an element trigger it's in-view classes earlier or later specifcy `aiv-offset` in pixels.
 
-```
+```html
 <div aiv aiv-offset="200">Toggle classes 200px down the page.</div>
 ```
 
@@ -48,7 +66,7 @@ To make an element trigger it's in-view classes earlier or later specifcy `aiv-o
 
 You can toggle classes on child elements by adding a css selector in the `aiv-children` property. This is best used with `aiv-delay` which will toggle child elements in sequence.
 
-```
+```html
 <ul aiv aiv-children="li" aiv-delay="200">
 	<li>Child element 1</li>
 	<li>Child element 2</li>
@@ -56,3 +74,25 @@ You can toggle classes on child elements by adding a css selector in the `aiv-ch
 </ul>
 ```
 
+### Start Visible
+
+By default, aiv will add the out classes to an element immediately upon loading to allow for page load animations. If you do not wish to use this set the `aiv-start_visible` paramater.
+
+```html
+<div aiv aiv-offset="200" aiv-start_visible>Toggle classes 200px down the page.</div>
+```
+
+### JS Api
+
+Temporary, to be rewritten
+
+```html
+<h3 class="js-api-example">JS API Example</h3>
+```
+
+```javascript
+aiv.add_nodes('.js-api-example',{
+	repeat: true,
+	delay: 500
+})
+```
--- src/aiv.js
+++ src/aiv.js
@@ -1,179 +1,197 @@
-const AnimateInView = {
-	prefix: 'aiv',
-	cls: {
-		in_view: 'in-view',
-		out_of_view: 'out-of-view',
-	},
-	throttle: 50,
-	last_call: false,
-	nodes: false,
-	init: function(context){
-		this.nodes = context.querySelectorAll('['+this.prefix+']');
-		if(!this.nodes) return;
-		this.scroll = this.scroll.bind(this)
-		this.resize = this.resize.bind(this)
-		this.add_view_classes = this.add_view_classes.bind(this)
-		this.add_initial_classes()
-		window.addEventListener('scroll', this.scroll)
-		window.addEventListener('resize', this.resize)
-		window.setTimeout(function(){
-			let event = document.createEvent("Event");
-			event.initEvent("scroll", false, true); 
-			if(window.pageXOffset == 0) window.dispatchEvent(event);
-		}, 60)
-		return this
-	},
-	destroy: function(){
-		this.nodes = false;
-		this.last_call = false;
-		window.removeEventListener('scroll', this.scroll)
-		window.removeEventListener('resize', this.resize)
-		window.removeEventListener('load', this.add_view_classes)
-	},
-	scroll: function(){
-		this.add_view_classes()
-	},
-	resize: function(){
-		this.add_view_classes()
-	},
-	add_initial_classes: function(){
-		this.nodes.forEach(function(node){
-			if(!node._className) node._className = '';
-			if(!node.attributes[this.prefix+'-start_visible']){
-				if(node.attributes[this.prefix+'-children']){
-					node.aiv_children = node.querySelectorAll(node.attributes[this.prefix+'-children'].nodeValue)
-					node.aiv_children.forEach(function(child_node){
-						child_node._className = '';
-						this.add_class(child_node, this.cls.out_of_view);
-						this.commit_class_changes(child_node);
-					}.bind(this))
-				} else {
-					this.add_class(node, this.cls.out_of_view);
-					this.commit_class_changes(node);
-				}
-			}
-		}.bind(this));
-	},
-	add_view_classes: function(){
-		const now = Date.now();
-		if(this.last_call && now - this.last_call < this.throttle) return;
-		this.last_call = now;
-		const visible_area = this.get_visible_area()
-		this.nodes.forEach(function(node){
+let nodes = [],
+	last_call = false,
+	opts = {};
 
-			// Skip if not a repeated animation and already triggered
-			if(node.status == 'in-view' && !node.attributes[this.prefix+'-repeat']) return;
-
-			const in_view = this.in_view(node, visible_area);
-
-			if(in_view && node.status !== 'in-view'){
-				node.status = 'in-view'
-				this.remove_class(node, this.cls.out_of_view)
-				this.add_class(node, this.cls.in_view)
-				if(node.attributes[this.prefix+'-cls']){
-					this.add_class(node, node.attributes[this.prefix+'-cls'].nodeValue)
-				}
-				if(node.attributes[this.prefix+'-out_cls']){
-					this.remove_class(node, node.attributes[this.prefix+'-out_cls'].nodeValue)
-				}
-				// Delay animation if set
-				if(node.attributes[this.prefix+'-delay'] && !node.timeout){
-					if(node.attributes[this.prefix+'-children']){
-						node.aiv_children.forEach(function(child_node, index){
-							child_node._className = node._className;
-							child_node.timeout = window.setTimeout(function(){
-								this.commit_class_changes(child_node);
-							}.bind(this), parseFloat(node.attributes[this.prefix+'-delay'].nodeValue) * index)
-						}.bind(this))
-					} else {
-						node.timeout = window.setTimeout(function(){
-							this.commit_class_changes(node);
-						}.bind(this), parseFloat(node.attributes[this.prefix+'-delay'].nodeValue))
-					}
-				} else {
-					// Or commit class changes
-					if(node.attributes[this.prefix+'-children']){
-						node.aiv_children.forEach(function(child_node){
-							child_node._className = node._className;
-							this.commit_class_changes(child_node);
-						}.bind(this))
-					} else {
-						this.commit_class_changes(node);
-					}
-				}
-
-			} else if(!in_view && node.status !== 'out-of-view') {
-				node.status = 'out-of-view'
-				// If currently inview then do out_cls when toggling out
-				if(this.has_class(node,this.cls.in_view) && node.attributes[this.prefix+'-out_cls']){
-					this.add_class(node, node.attributes[this.prefix+'-out_cls'].nodeValue)
-				}
-				this.remove_class(node, this.cls.in_view)
-				this.add_class(node, this.cls.out_of_view)
-				if(node.attributes[this.prefix+'-cls']){
-					this.remove_class(node, node.attributes[this.prefix+'-cls'].nodeValue)
-				}
-				// Clear delayed animation if set
-				if(node.attributes[this.prefix+'-children']){
-					node.aiv_children.forEach(function(child_node, index){
-						child_node._className = node._className;
-						if(child_node.timeout){
-							window.clearTimeout(child_node.timeout)
-							child_node.timeout = false
-						}
-						this.commit_class_changes(child_node);
-					}.bind(this))
-				} else {
-					if(node.timeout){
-						window.clearTimeout(node.timeout)
-						node.timeout = false
-					}
-					this.commit_class_changes(node);
-				}
-			}
-
-		}.bind(this))
-	},
-	has_class: function(node, cls){
-		return !!node._className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-	},
-	add_class: function(node, cls){
-		if(!this.has_class(node, cls)) node._className = (node._className+" "+cls).trim();
-		return node;
-	},
-	remove_class: function(node, cls){
-		if(this.has_class(node,cls)) {
-			node._className = node._className.replace(new RegExp('(\\s|^)'+cls+'(\\s|$)'), ' ').trim();
-		}
-		return node;
-	},
-	commit_class_changes: function(node){
-		if(node._className !== node.className){
-			node.className = node._className;
-			node._className = node.className;
-		}
-	},
-	in_view: function(node, visible_area){
-		// I think this is wrong
-		let top = node.offsetTop + node.offsetHeight;
-		let bottom = node.offsetTop;
-		if(node.attributes[this.prefix+'-offset']) {
-			top += parseFloat(node.attributes[this.prefix+'-offset'].nodeValue);
-			bottom += parseFloat(node.attributes[this.prefix+'-offset'].nodeValue);
-		}
-		return top > visible_area.top && bottom < visible_area.bottom;
-	},
-	get_visible_area: function(){
-		return {top: window.pageYOffset, bottom: window.pageYOffset + window.innerHeight}
+// Update options
+function configure(_opts){
+	for(opt in _opts){
+		opts[opt] = _opts[opt]
 	}
 }
 
-if ( !NodeList.prototype.forEach ) {
-  NodeList.prototype.forEach = function(fn, scope) {
-    for (var i = 0, len = this.length; i < len; ++i) {
-      fn.call(scope || this, this[i], i, this);
-    }
-  }
+// Add items to context
+function add(ctx){
+	nodes = nodes.concat(Array.prototype.slice.call(ctx.querySelectorAll('[aiv]')))
 }
 
-module.exports = Object.create(AnimateInView).init(document.querySelector('body'));
+// Remove node from list to update
+function remove(node){
+	if(node.aiv.timeout) window.clearTimeout(node.aiv.timeout)
+	nodes = nodes.filter(_node => _node !== node)
+}
+
+// Bind scroll and resize events to update toggles
+function bind(ctx){
+	ctx.addEventListener('scroll', update)
+	ctx.addEventListener('resize', update)
+	ctx.setTimeout(function(){
+		let event = document.createEvent("Event");
+		event.initEvent("scroll", false, true); 
+		if(ctx.pageXOffset == 0) ctx.dispatchEvent(event);
+	}, 60)
+}
+
+// Unbind events
+function unbind(ctx){
+	ctx.removeEventListener('scroll', update)
+	ctx.removeEventListener('resize', update)
+}
+
+// Setup before first update
+function load(){
+	let _nodes = nodes;
+	for(i in _nodes) load_node(_nodes[i])
+}
+
+// Sets up initial classes on nodes
+function load_node(node, options){
+
+	node.aiv = {
+		triggered: false,
+		cls : get_attr(node, 'cls', false),
+		out_cls : get_attr(node, 'out_cls', false),
+		children : get_attr(node, 'children', false),
+		repeat : get_attr(node, 'repeat', false, false, true),
+		start_visible : get_attr(node, 'start_visible', false, false, true),
+		delay : get_attr(node, 'delay', 0, true),
+		offset : get_attr(node, 'offset', 0, true),
+	}
+
+	if(options){
+		for(opt in options){
+			node.aiv[opt] = options[opt]
+		}
+	}
+
+	if(node.aiv.children) node.aiv.children = Array.prototype.slice.call(node.querySelectorAll(node.aiv.children));
+
+	console.log(node, node.aiv)
+
+	const to_change = node.aiv.children ? node.aiv.children : [node]
+
+	let in_cls = [opts.in_cls];
+	if(node.aiv.cls) in_cls.push(node.aiv.cls)
+	let out_cls = [opts.out_cls];
+	if(node.aiv.out_cls) out_cls.push(node.aiv.out_cls)
+
+	for(index in to_change){
+
+		let target_node = to_change[index];
+		if(!target_node.aiv) target_node.aiv = {}
+
+		// Store active and inactive classes. Append the current className
+		target_node.aiv.in_cls = in_cls.slice();
+		target_node.aiv.out_cls = out_cls.slice();
+		target_node.aiv.in_cls.push(target_node.className)
+		target_node.aiv.out_cls.push(target_node.className)
+
+		// Don't set initial classes on start_visible nodes
+		if(!node.aiv.start_visible){
+			className(target_node, target_node.aiv.out_cls)
+		}
+
+	}
+}
+
+// Update all nodes
+function update(){
+	const now = Date.now();
+	if(last_call && now - last_call < opts.throttle) return;
+	last_call = now;
+	const visible = visible_area()
+	let _nodes = nodes;
+	for(i in _nodes) toggle(_nodes[i], visible)
+}
+
+// Toggle statuses on a single node
+function toggle(node, visible){
+
+	const in_view = is_in_view(node, visible);	
+	const to_change = node.aiv.children ? node.aiv.children : [node]
+
+	for(index in to_change){
+		// Set classes if in
+		let target_node = to_change[index];
+		if(in_view && target_node.aiv_status !== 'in'){
+			target_node.aiv_status = 'in';
+			node.aiv.triggered = true;
+			if(node.aiv.delay){
+				target_node.aiv.timeout = window.setTimeout(function(){
+					className(target_node, target_node.aiv.in_cls)
+				}, node.aiv.delay * (node.aiv.children ? index : index + 1))
+			} else {
+				className(target_node, target_node.aiv.in_cls)
+			}
+		}
+		// Set classes if out
+		if(!in_view && target_node.aiv_status !== 'out'){
+			// Remove node if it should not repeat
+			if(target_node.aiv.timeout){
+				window.clearTimeout(target_node.aiv.timeout)
+				target_node.aiv.timeout = false
+			}
+			if(node.aiv.triggered && !node.aiv.repeat){
+				remove(node)
+				continue;
+			}
+			target_node.aiv_status = 'out';
+			className(target_node, target_node.aiv.out_cls)
+		}
+	}
+
+}
+
+// Changes classes to stored _className
+function className(node, cls){
+	node.className = cls.join(' ');
+}
+
+// Get attribute values with defaults
+function get_attr(node, attr, def, number, toggle){
+	const att = node.attributes['aiv-'+attr];
+	if(!att) return def;
+	if(toggle) return true;
+	if(number) return parseInt(att.nodeValue);
+	return att.nodeValue;
+}
+
+//
+function is_in_view(node, visible){
+	return (node.offsetTop + node.aiv.offset) <= visible.bottom && (node.offsetTop + node.offsetHeight + node.aiv.offset) >= visible.top;
+}
+
+// Get visible area in window
+function visible_area(){
+	let y = parseInt(window.pageYOffset);
+	return {top: y, bottom: y + parseInt(window.innerHeight)}
+}
+
+// JS API
+function add_nodes(selector, options){
+	let _nodes = Array.prototype.slice.call(document.querySelectorAll(selector));
+	for(i in _nodes) load_node(_nodes[i], options)
+	nodes = nodes.concat(_nodes)
+}
+
+// Set default options
+function init(options, context, win){
+	configure(options || {
+		in_cls: 'in-view',
+		out_cls: 'out-of-view',
+		throttle: 50
+	})
+	add(context || document.body);
+	load();
+	bind(win || window);
+}
+
+module.exports = {
+	configure: configure,
+	load: load,
+	add: add,
+	add_nodes: add_nodes,
+	remove: remove,
+	bind: bind,
+	unbind: unbind,
+	init: init
+};
